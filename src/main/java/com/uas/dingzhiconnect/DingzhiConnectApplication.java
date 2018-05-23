b246d72fd36ab3d51e4bcb41fce3a8cd633ea850
guq <865371097@qq.com>
2018-05-23 20:28:09 +0800

    add

---
+++ src/main/java/com/uas/dingzhiconnect/DingzhiConnectApplication.java
@@ -0,0 +1,14 @@
+package com.uas.dingzhiconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class DingzhiConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingzhiConnectApplication.class, args);
+	}
+}
